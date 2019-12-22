c9691f7f54ed2b300f33f9400c0c235971d24b00
Faissal <faissal.lauar@versata.com]>
2019-12-22 20:13:11 -0300

    installed node name

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('NodeJS example via job DSL') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
@@ -9,7 +9,7 @@ job('NodeJS example') {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('node-8') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
